c44b27ae51351e4d0454a18f8f63fa0764c6b10c
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-12-16 15:24:28 +0000

    return 404 if token not found

--- app/controllers/service_token_v2_controller.rb
+++ app/controllers/service_token_v2_controller.rb
@@ -2,6 +2,10 @@ class ServiceTokenV2Controller < ApplicationController
   def show
     token = Support::ServiceTokenAuthoritativeSource.get_public_key(service_slug: params[:service_slug])
 
-    render json: {token: token}, status: 200
+    if token.present?
+      render json: { token: token }, status: 200
+    else
+      head :not_found
+    end
   end
 end
--- spec/controllers/service_token_v2_controller_spec.rb
+++ spec/controllers/service_token_v2_controller_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe ServiceTokenV2Controller do
     end
 
     context 'when service does not exist' do
+      it 'returns 404' do
+        allow(Support::ServiceTokenAuthoritativeSource).to receive(:get_public_key).and_return('')
+
+        get :show, params: { service_slug: 'test-service' }
+        expect(response).to be_not_found
+      end
     end
   end
 end
