1960d3ccb6c61188d414484368ce388715b334d3
Phil Lee <phil.lee@digital.justice.gov.uk>
2020-01-30 10:46:32 +0000

    remove v1 endpoint

--- app/services/support/service_token_authoritative_source.rb
+++ app/services/support/service_token_authoritative_source.rb
@@ -1,33 +1,7 @@
 class Support::ServiceTokenAuthoritativeSource
-  def self.get(service_slug)
-    Adapters::KubectlAdapter.get_platform_secret(platform_secret_name(service_slug)) || Adapters::KubectlAdapter.get_service_secret(service_secret_name(service_slug))
-  end
-
-  def self.get_service_secret(slug)
-    Adapters::KubectlAdapter.get_secret(service_secret_name(service_slug))
-  end
-
-  def self.get_platform_secret(slug)
-    Adapters::KubectlAdapter.get_secret(platform_secret_name(service_slug))
-  end
-
   def self.get_public_key(service_slug:)
     adapter = Adapters::KubectlAdapter.new(secret_name: service_slug,
                                            namespace: ENV['KUBECTL_SERVICES_NAMESPACE'])
     adapter.get_public_key
-  end
-
-  def self.service_secret_name(service_slug)
-    ['fb-service', service_slug, 'token', environment_slug].join('-')
-  end
-
-  def self.platform_secret_name(service_slug)
-    ['fb-platform', service_slug, 'token', environment_slug].join('-')
-  end
-
-  private
-
-  def self.environment_slug
-    ENV['FB_ENVIRONMENT_SLUG']
   end
 end
--- spec/services/adapters/kubectl_adapter_spec.rb
+++ spec/services/adapters/kubectl_adapter_spec.rb
@@ -12,60 +12,6 @@ describe Adapters::KubectlAdapter do
     described_class.new(secret_name: secret_name, namespace: namespace)
   end
 
-  describe '#get_secret' do
-    let(:kubectl_output) { 'kubectl output]' }
-    let(:parsed_json) do
-      {
-        'data' => {
-          'a_key' => 'a value',
-          'another_key' => 'another value',
-          'token' => 'token value'
-        }
-      }
-    end
-
-    before do
-      allow(JSON).to receive(:parse).with(kubectl_output).and_return(parsed_json)
-      allow(Base64).to receive(:decode64).with('token value').and_return('decoded token')
-      allow(subject).to receive(:kubectl_cmd).and_return('kubectl cmd')
-      allow(Adapters::ShellAdapter).to receive(:output_of).with('kubectl cmd').and_return(kubectl_output)
-    end
-
-    it 'calls kubectl_cmd passing the given secret name' do
-      expect(subject).to receive(:kubectl_cmd)
-      subject.get_secret
-    end
-
-    it 'gets the output of the kubectl_cmd' do
-      expect(Adapters::ShellAdapter).to receive(:output_of).with('kubectl cmd').and_return(kubectl_output)
-      subject.get_secret
-    end
-
-    it 'parses the kubectl output as JSON' do
-      expect(JSON).to receive(:parse).with(kubectl_output).and_return(parsed_json)
-      subject.get_secret
-    end
-
-    it 'base64-decodes the [data][token] key' do
-      expect(Base64).to receive(:decode64).with('token value').and_return('decoded token')
-      subject.get_secret
-    end
-
-    it 'returns the decoded token' do
-      expect(subject.get_secret).to eq('decoded token')
-    end
-
-    context 'when shell adapter retruns empty string' do
-      before :each do
-        expect(Adapters::ShellAdapter).to receive(:output_of).with('kubectl cmd').and_return('')
-      end
-
-      it 'returns nil' do
-        expect(subject.get_secret).to be_nil
-      end
-    end
-  end
-
   describe '#get_public_key' do
     context 'when code injection' do
       context 'with dangerous secret_name' do
